dd9d36108da530ea20842f30c098a68d46a77cea
Jim <jamrsim@users.noreply.github.com>
2021-08-02 14:59:17 -0400

    Update index.js
    Fix issue I logged.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
