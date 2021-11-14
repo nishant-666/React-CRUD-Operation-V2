16fbf7d54afad561b46f24ab05bae91b6a7486d6
akshaysoni8707 <akshay.soni.8707@gmail.com>
2021-11-14 12:06:38 +0530

    links, routing, components updated

--- src/components/create.js
+++ src/components/create.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
 import axios from 'axios';
 import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export default function Create() {
     let history = useHistory();
@@ -15,11 +16,16 @@ export default function Create() {
             lastName,
             checkbox
         }).then(() => {
-            history.push('/read')
+            history.push('/')
         })
     }
     return (
         <div>
+            <div style={{ marginTop: 20,marginBottom: 20 }}>
+                <Link to="/">
+                    <Button color='violet'>Home</Button>
+                </Link>
+            </div>
             <Form className="create-form">
                 <Form.Field>
                     <label>First Name</label>
@@ -32,7 +38,7 @@ export default function Create() {
                 <Form.Field>
                     <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
                 </Form.Field>
-                <Button onClick={postData} type='submit'>Submit</Button>
+                <Button color="green" onClick={postData} type='submit'>Submit</Button>
             </Form>
         </div>
     )
